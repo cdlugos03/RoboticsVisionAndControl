3dbddecc01ced82d99e7b6d53332639fdb9ec1c6
cdlugos03 <113647528+cdlugos03@users.noreply.github.com>
2024-07-14 23:08:36 -0400

    Update 2.2 Working in Two Dimensions - Rotational Matricies.py

--- 2.2 Working in Two Dimensions - Rotational Matricies.py
+++ 2.2 Working in Two Dimensions - Rotational Matricies.py
@@ -1,3 +1,7 @@
+#This program will show you the basics of a rotational matrix
+#using python, the applications for this could determine precise 
+#rotational movement of a robot with 2 dimensions of movement
+
 import numpy as np
 import math
 
@@ -7,15 +11,15 @@ def rot2(theta):
         [np.sin(theta), np.cos(theta)]
     ])
 
-
-
-# Example usage
+#USAGE
 theta = (1.5708)  # angle in radians
-R = rot2(theta)
+R = rot2(theta) #pass theta to the rotational matrix
 print(R)
 print()
 
-start_pos = np.array([1,0])
+start_pos = np.array([1,0]) #multiply starting matrix by rotational matrix to determin
+#final 2D position
+
 end_pos = R @ start_pos
 
 print(end_pos)
